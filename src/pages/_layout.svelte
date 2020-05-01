712147a342a842cdcfb43fc75190f31b0f7bf3c7
jurkat <github@gurkat.io>
2020-05-01 12:35:29 +0200

    added svelt-config and in-file scss

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,1 +1,18 @@
+<style lang="scss">
+main {
+   text-align: center;
+   padding: 1em;
+   max-width: 240px;
+   margin: 0 auto;
+   &:hover {
+      background: blue;
+   }
+}
+
+@media (min-width: 640px) {
+   main {
+      max-width: none;
+   }
+}
+</style>
 <main><slot/></main>
