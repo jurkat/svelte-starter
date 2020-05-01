f2e32445997a5f3546b6888ea64f01dbbc8b9e49
jurkat <56468822+jurkat@users.noreply.github.com>
2020-05-01 12:41:21 +0200

    Initial working template (#1)
    * init with official svelte starter template
    
    * Updated to newest versions
    
    * Added svelte-preprocess
    
    * Added Routify
    
    * added svelt-config and in-file scss
    
    * removed blue on hover
    
    Co-authored-by: jurkat <github@gurkat.io>

---
+++ src/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Router } from "@sveltech/routify";
+  import { routes } from "@sveltech/routify/tmp/routes";
+</script>
+
+<style global src="./main.scss"></style>
+
+<Router {routes} />
---
+++ src/pages/about.svelte
@@ -0,0 +1,2 @@
+<h1>About the World!</h1>
+<p>Visit the <a href="/">Index Page</a>.</p>
